a83d22725f685417af2c72d55357ce47023c1788
hussain-anjum <haratul14@gmail.com>
2025-10-23 17:58:33 +0600

    Add dataTypes.js

---
+++ dataTypes.js
@@ -0,0 +1,21 @@
+// number
+// bigInt
+// string -> "string"
+// boolean -> true/false
+// null -> object but it is a standalone value
+// undefined
+// symbol -> unique
+
+//object
+
+let userId = 1001;
+const userName = "har";
+let isLoggedIn = true;
+let loc;
+let userAdd = null;
+
+console.log(typeof userId);
+console.log(typeof userName);
+console.log(typeof isLoggedIn);
+console.log(typeof loc);
+console.log(typeof userAdd); // null is a standalone value, typeof return value is object
