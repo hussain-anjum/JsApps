d353870608d3a907f8bc3713f8f9cfeeae7961f7
hussain-anjum <haratul14@gmail.com>
2025-10-25 19:48:51 +0600

    Practice timestamp and date Obj methods

--- basics_01/dates.js
+++ basics_01/dates.js
@@ -7,13 +7,33 @@ let myDate = new Date();
 // console.log(myDate.toLocaleDateString()); //10/25/2025
 
 let myOwnDate = new Date(2025, 0, 25); // in this form month starts from 0 = Jan in Js
-console.log(myOwnDate.toLocaleString()); // 1/25/2025, 12:00:00 AM
+// console.log(myOwnDate.toLocaleString()); // 1/25/2025, 12:00:00 AM
 
 let myOwnDate2 = new Date(2025, 0, 0);
-console.log(myOwnDate2.toLocaleString()); // 12/31/2024, 12:00:00 AM
+// console.log(myOwnDate2.toLocaleString()); // 12/31/2024, 12:00:00 AM
 
 let myCreatedDate = new Date(2025, 0, 23, 5, 3); // 1/23/2025, 5:03:00 AM
-let myCreatedDate2 = new Date("2023-01-14");
+let myCreatedDate2 = new Date("2023-01-14"); // 1/14/2023; in this case month starts with 1, not 0.
 
-console.log(myCreatedDate.toLocaleString());
-console.log(myCreatedDate2.toLocaleDateString()); // 1/14/2023
+// console.log(myCreatedDate.toLocaleString());
+// console.log(myCreatedDate2.toLocaleDateString());
+
+let myTimeStamp = Date.now(); // gives the present time in milli seconds
+
+console.log(myTimeStamp); // 1761384394580 (ms value)
+
+console.log(myCreatedDate.getTime()); // it returns the ms value as a time of that date
+console.log(Math.floor(Date.now() / 1000)); // returns present time in sec.
+
+// ++++++++++++++++ Do comparision in ms ++++++++++++++++++++
+
+let newDate = new Date();
+
+console.log(newDate.getMonth() + 1); // return current month
+console.log(newDate.getDay()); // return current day(starts from MONDAY = 0)
+console.log(newDate.getFullYear()); // return current year
+
+let dayName = newDate.toLocaleString("default", {
+  weekday: "long",
+});
+console.log(dayName);
