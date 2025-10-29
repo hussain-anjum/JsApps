da91741c5d0d7fcbfde18a86dd96b4411a0b1548
hussain-anjum <haratul14@gmail.com>
2025-10-29 23:06:01 +0600

    Add object and array handle fuctions

--- basics_03/functions_1.js
+++ basics_03/functions_1.js
@@ -26,7 +26,7 @@ function sum2(num1, num2) {
 }
 // console.log("Result =", sum2(5, 3));
 
-function loginMessage(userName = "No one") {
+function loginMessage(userName) {
   // !userName ≈ userName === undefined || userName === null || userName === ""
   // means if userName is falsy (such as undefined, null, "", 0, false, etc.), then the if block will execute.
   if (!userName) {
@@ -37,4 +37,59 @@ function loginMessage(userName = "No one") {
 }
 // console.log(loginMessage("har"));    // har just logged in
 // console.log(loginMessage()); // undefined just logged in (it will show without if condition)
-console.log(loginMessage()); // Please enter a valid userName (it will show with if condition)
+// console.log(loginMessage()); // Please enter a valid userName (it will show with if condition)
+
+function greetings(user = "Unknown") {
+  return `Welcome! ${user}`;
+}
+// console.log(greetings("har")); //Welcome! har
+
+// if we don't pass any attribute Unknown will be the default user.
+// console.log(greetings()); //Welcome! Unknown;
+
+// Imagine shopping cart:
+
+// ###################      rest parameter(...)      ############################
+function calculateCartPrice(...num1) {
+  return num1;
+}
+console.log(calculateCartPrice(200, 400, 500, 2000)); //[ 200, 400, 500, 2000 ] if we need to pass unlimited argument, we use rest(...) parameter
+
+function calculateCartPrice2(val1, val2, ...num1) {
+  return num1;
+}
+// The first two parameters (val1, val2) will receive their values individually.
+// All the remaining values will be collected into an array inside ...num1.
+
+console.log(calculateCartPrice2(200, 400, 500, 2000)); // [ 500, 2000 ]
+
+// ###################       Handle Object      ########################
+
+const user = {
+  username: "har",
+  price: 199,
+};
+
+function handleObject(anyobject) {
+  console.log(
+    `Username is ${anyobject.username} and price is ${anyobject.price}`
+  );
+}
+handleObject(user);
+// we can pass direct obj:
+handleObject({
+  username: "abul",
+  price: 399,
+});
+
+// ###################       Handle Arry      ########################
+
+const myArray = [200, 400, 100, 600];
+
+function returnSecondValue(getArray) {
+  return getArray[1];
+}
+
+console.log(returnSecondValue(myArray));
+// we can pass direct array:
+console.log(returnSecondValue([100, 300, 500, 1000]));
