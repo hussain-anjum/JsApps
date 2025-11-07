502aa76fab03b91766caaca75c5a7a1379a97acc
hussain-anjum <haratul14@gmail.com>
2025-11-07 19:20:36 +0600

    Implement control flow examples

---
+++ controlFlow/switch.js
@@ -0,0 +1,29 @@
+// switch (key) {
+//     case value:
+
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "march";
+
+switch (month) {
+  case "jan":
+    console.log("Jan");
+    break;
+  case "feb":
+    console.log("Feb");
+    break;
+  case "march":
+    console.log("Mar");
+    break;
+  case "april":
+    console.log("Apr");
+    break;
+
+  default:
+    console.log("default case match");
+    break;
+}
