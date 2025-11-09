3e8020e6d5f62c4023385cdd8e98d1486acbb96a
hussain-anjum <haratul14@gmail.com>
2025-11-09 19:51:03 +0600

    Enhance map method explanation

--- iterations/map.js
+++ iterations/map.js
@@ -2,6 +2,7 @@ const myNumers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 // The map() method of Array instances creates a new array populated with the results of calling a provided function
 // on every element in the calling array.
+// Using map, we just do some operation on each element of the array and return a new array.
 
 // const newNums = myNumers.map((num) => {
 //   return num + 10;
