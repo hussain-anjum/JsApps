2954ef99e64bb27e142f88ea6eefc6223a5952d4
hussain-anjum <haratul14@gmail.com>
2025-11-09 19:48:58 +0600

    Refactor filter example

--- iterations/filter.js
+++ iterations/filter.js
@@ -16,18 +16,18 @@ const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 //const newNums = myNums.filter((num) => num > 4); // shorthand arrow function. filter also take callback function like forEach
 // console.log(newNums);    // [ 5, 6, 7, 8, 9, 10 ]
 
-// const newNums = myNums.filter((num) => {
-//   return num > 4;
-// });
+const newNums = myNums.filter((num) => {
+  return num > 4;
+});
 // console.log(newNums); //[ 5, 6, 7, 8, 9, 10 ]
 
 // Without using filter() method:
-const newNums = [];
-myNums.forEach((num) => {
-  if (num > 4) {
-    newNums.push(num);
-  }
-});
+// const newNums = [];
+// myNums.forEach((num) => {
+//   if (num > 4) {
+//     newNums.push(num);
+//   }
+// });
 // console.log(newNums); //[ 5, 6, 7, 8, 9, 10 ]
 
 // ++++++++++++++++++   Example:  ++++++++++++++++++
