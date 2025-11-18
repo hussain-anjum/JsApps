d06760e7e211efab7f8c71b361810b50dd9cc655
hussain-anjum <haratul14@gmail.com>
2025-11-18 22:39:11 +0600

    Try with switch case

--- projects/Color_Switcher/script.js
+++ projects/Color_Switcher/script.js
@@ -18,5 +18,21 @@ buttons.forEach(function (button) {
     if (e.target.id === "yellow") {
       body.style.backgroundColor = e.target.id;
     }
+    // switch (e.target.id) {
+    //   case "grey":
+    //     body.style.backgroundColor = e.target.id;
+    //     break;
+    //   case "white":
+    //     body.style.backgroundColor = e.target.id;
+    //     break;
+    //   case "blue":
+    //     body.style.backgroundColor = e.target.id;
+    //     break;
+    //   case "yellow":
+    //     body.style.backgroundColor = e.target.id;
+    //     break;
+    //   default:
+    //     body.style.backgroundColor = "white";
+    // }
   });
 });
